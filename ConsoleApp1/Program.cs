759edbe05ef4586541fed24d7ff03aac57039c64
pbansal16 <prateekbansalind@gmail.com>
2021-10-10 20:56:30 +1300

    Initial Commit - Program to identify whether the number is odd or even.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program to identify whether the output is even or odd
+
+            int number = 0;
+            Console.Write("Enter a number: ");
+            string numberText = Console.ReadLine();
+
+            number = int.Parse(numberText);
+
+
+            if (number %  2 == 0)
+            {
+                Console.WriteLine($"Number {number} is even.");
+            }
+            else 
+            {
+                Console.WriteLine($"Number {number} is odd.");
+            }
+            
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
